670717c70ea88fdd2e3a239a93907e97a92d9c98
DIAMANTE SIMONE CRESCENZO s295783 <s295783@studenti.polito.it>
2022-05-11 12:51:25 +0200

    Added: USER APIs, first implementation, no session management yet. Updated TimeSheet.md

--- code/server/User/UserDAO.js
+++ code/server/User/UserDAO.js
@@ -26,50 +26,6 @@ class UserDAO {
         console.log('newUserTable() CALLED')
     }
 
-    newUser(user){
-
-        //console.log(user.getUserAsDict());
-        
-        return new Promise((resolve, reject) => {
-
-            let insert = true;
-
-            // query to DB to check if tuple USERNAME-TYPE is already present
-            const sql1 = 'SELECT * FROM USERS WHERE EMAIL = ? AND TYPE = ?';
-            this.#db.getDB().all(sql1, [user.getEmail(), user.getType()], (err, rows) => {
-                if(err){
-                    // reports error while querying database
-                    console.log('newUser() sql1.run error:: ', err);
-                    reject(503);    // 503 Service Unavailable (generic error)
-                    insert = false;
-                    return;
-                }
-                //console.log(rows);
-                // if tuples are found, new USER cannot be accepted
-                if(rows.length != 0){
-                    reject(409);    // 409 Conflict
-                    insert = false;
-                    return;
-                }
-
-                // query to DB to insert new USER
-                const sql2 = 'INSERT INTO USERS(NAME, SURNAME, EMAIL, PASSWORD, TYPE) VALUES (?, ?, ?, ?, ?)';
-                this.#db.getDB().run(sql2, [user.getName(), user.getSurname(), user.getEmail(), user.getPassword(), user.getType()], (err) => {
-                    if (err) {
-                        // reports error while querying database
-                        console.log('newUser() sql2.run error:: ', err);
-                        reject(503);    // 503 Service Unavailable (generic error)
-                        return;
-                    }
-                    // USER added successfully
-                    resolve(201);
-                    return;
-                });
-            });
-        });
-
-    }
-
     getUser(data){
         return new Promise((resolve, reject) => {
             let sampleUser = {
@@ -128,8 +84,8 @@ class UserDAO {
             let loggedAndAuthorized = true;
             if(loggedAndAuthorized){
 
-                sql = 'SELECT * FROM USERS WHERE TYPE=\'manager\''
-                sql.all(sql, (err, rows) => {
+                let sql = 'SELECT * FROM USERS WHERE TYPE=\'supplier\''
+                this.#db.getDB().all(sql, (err, rows) => {
                     if(err){
                         // reports error while querying database
                         console.log('getUser() sql.run error:: ', err);
@@ -143,14 +99,13 @@ class UserDAO {
                         resolve(rows);  // returns empty array
                         return;
                     } else {
-                        // collects user data into a dictionary
+                        // collects suppliers data into a dictionary
                         const userData = rows.map((r) => (
                             {
                                 id:r.ID,
                                 username:r.EMAIL,
                                 name:r.NAME,
-                                surname:r.SURNAME,
-                                type:r.TYPE
+                                surname:r.SURNAME
                             }
                         ));
 
@@ -167,6 +122,200 @@ class UserDAO {
 
         });
     }
+
+    getUsers() {
+        return new Promise((resolve, reject) => {
+
+            // !!! session checking to be implemented !!!
+            let loggedAndAuthorized = true;
+            if(loggedAndAuthorized) {
+
+                // query to DB to retrieve all users (except manager)
+                let sql = 'SELECT * FROM USERS WHERE NOT (TYPE=\'manager\')';
+                this.#db.getDB().all(sql, (err, rows) => {
+                    if(err){
+                        // reports error while querying database
+                        console.log('getUser() sql.run error:: ', err);
+                        reject(500);    // 500 Internal Server Error (generic error)
+                        return;
+                    }
+                    //console.log(rows);
+
+                    // collects user data into a dictionary (except manager)
+                    const users = rows.map((r) => (
+                        {
+                            id:r.ID,
+                            name:r.NAME,
+                            surname:r.SURNAME,
+                            email:r.EMAIL,
+                            type:r.TYPE
+                        }
+                    ));
+                    //console.log(users);
+
+                    resolve(users);
+                    return;
+                });
+            } else {
+                reject(401);    // 401 Unauthorized
+                return;
+            }
+        });
+    }
+
+    newUser(user){
+
+        //console.log(user.getUserAsDict());
+        
+        return new Promise((resolve, reject) => {
+
+            // !!! session checking to be implemented !!!
+            let loggedAndAuthorized = true;
+            if(loggedAndAuthorized) {
+                // query to DB to check if tuple USERNAME-TYPE is already present
+                const sql1 = 'SELECT * FROM USERS WHERE EMAIL = ? AND TYPE = ?';
+                this.#db.getDB().all(sql1, [user.getEmail(), user.getType()], (err, rows) => {
+                    if(err){
+                        // reports error while querying database
+                        console.log('newUser() sql1.run error:: ', err);
+                        reject(503);    // 503 Service Unavailable (generic error)
+                        return;
+                    }
+                    //console.log(rows);
+                    // if tuples are found, new USER cannot be accepted
+                    if(rows.length != 0){
+                        reject(409);    // 409 Conflict
+                        return;
+                    }
+
+                    // query to DB to insert new USER
+                    const sql2 = 'INSERT INTO USERS(NAME, SURNAME, EMAIL, PASSWORD, TYPE) VALUES (?, ?, ?, ?, ?)';
+                    this.#db.getDB().run(sql2, [user.getName(), user.getSurname(), user.getEmail(), user.getPassword(), user.getType()], (err) => {
+                        if (err) {
+                            // reports error while querying database
+                            console.log('newUser() sql2.run error:: ', err);
+                            reject(503);    // 503 Service Unavailable (generic error)
+                            return;
+                        }
+                        // USER added successfully
+                        resolve(201);
+                        return;
+                    });
+                });
+            } else {
+                reject(401); // 401 Unauthorized
+                return;
+            }
+        });
+
+    }
+
+    userSession(userData) {
+        return new Promise((resolve, reject) => {
+
+            // !!! session checking to be implemented !!!
+            let loggedAndAuthorized = true;
+            if(loggedAndAuthorized) {
+
+                let sql = 'SELECT * FROM USERS WHERE EMAIL=? AND PASSWORD=? AND TYPE=?';
+                this.#db.getDB().all(sql, [userData.username, userData.password, userData.type], (err, rows) => {
+                    if(err){
+                        // reports error while querying database
+                        console.log('userSession() sql.all error:: ', err);
+                        reject(500);    // 500 Internal Server Error (generic error)
+                        return;
+                    } else if (rows.length == 0) {
+                        // rejects session if no USER corresponding to data is found
+                        reject(401);    // 401 Unauthorized
+                        return;
+                    } else {
+                        // prepares response body (customer info: id, username, name)
+                        const customerInfo = rows.map((r) => (
+                            {
+                                id:r.ID,
+                                username:r.EMAIL,
+                                name:r.NAME
+                            }
+                        ));
+                        resolve(customerInfo);
+                        return;
+                    }
+                });
+            } else {
+                reject(401);    // 401 Unauthorized
+                return;
+            }
+        });
+    }
+
+    modifyUserType(newUserData) {
+        return new Promise((resolve, reject) => {
+            // !!! session checking to be implemented !!!
+            let loggedAndAuthorized = true;
+            if(loggedAndAuthorized) {
+
+                let sql1 = 'SELECT * FROM USERS WHERE EMAIL=? AND TYPE=?';
+                this.#db.getDB().all(sql1, [newUserData.username, newUserData.oldType], (err, rows) => {
+                    if(err){
+                        // reports error while querying database
+                        console.log('modifyUserType() sql1.all error:: ', err);
+                        reject(500);    // 500 Internal Server Error (generic error)
+                        return;
+                    } else if (rows.length == 0) {
+                        // wrong username or oldType fields or user doesn't exists
+                        reject(404);    // 404 Not Found
+                        return;
+                    } else {
+                        let userID = rows.map((r) => r.ID);
+                        userID = userID[0];
+                        console.log(userID);
+
+                        let sql2 = 'UPDATE USERS SET TYPE=? WHERE ID=?';
+                        this.#db.getDB().all(sql2, [newUserData.newType, userID], (err, rows) => {
+                            if(err){
+                                // reports error while querying database
+                                console.log('modifyUserType() sql2.all error:: ', err);
+                                reject(500);    // 500 Internal Server Error (generic error)
+                                return;
+                            } else {
+                                resolve(200);   // 200 OK
+                                return;
+                            }
+                        });
+                    }
+                });
+
+            } else {
+                reject(401);    // 401 Unauthorized
+                return
+            }
+        });
+    }
+
+    deleteUser(userData) {
+        return new Promise((resolve, reject) => {
+            // !!! session checking to be implemented !!!
+            let loggedAndAuthorized = true;
+            if(loggedAndAuthorized) {
+                let sql = 'DELETE FROM USERS WHERE EMAIL=? AND TYPE=?';
+                this.#db.getDB().run(sql, [userData.username, userData.type], (err) => {
+                    if(err){
+                        // reports error while querying database
+                        console.log('deleteUser() sql.all error:: ', err);
+                        reject(503);    // 500 Service Unavailable (generic error)
+                        return;
+                    } else {
+                        resolve(204);   // 204 No Content (success)
+                        return;
+                    }
+                });
+            } else {
+                reject(401);    // 401 Unauthorized
+                return;
+            }
+        });
+    }
+
 }
 
 module.exports = UserDAO;
--- code/server/server.js
+++ code/server/server.js
@@ -1,4 +1,5 @@
 'use strict';
+const { use } = require('chai');
 const express = require('express');
 // init express
 const app = new express();
@@ -21,6 +22,8 @@ let test_user = new User();
 test_user.getId();
 new UserDAO().newUserTable();
 
+const userTypes = ['customer', 'qualityEmployee', 'clerk', 'deliveryEmployee', 'supplier'];
+
 // ***********************************
 
 //GET /api/test
@@ -38,11 +41,11 @@ app.get('/api/test', (req,res)=>{
 });
 
 //GET /api/userinfo
-app.get('/api/userinfo', (req, res) => {
+app.get('/api/userinfo', async (req, res) => {
   console.log('GET /api/userinfo');
 
-  let getUserInfoPromise = new UserDAO(firstDBtest).getUser();
-  getUserInfoPromise.then(
+  let tmpUserDAO = new UserDAO(firstDBtest);
+  await tmpUserDAO.getUser().then(
     function(value) {
       console.log('getUser resolve');
       return res.status(200).json(value).end();
@@ -59,12 +62,12 @@ app.get('/api/userinfo', (req, res) => {
 });
 
 //GET /api/suppliers
-app.get('/api/suppliers', (req, res) => {
+app.get('/api/suppliers', async (req, res) => {
 
   console.log('GET /api/suppliers');
 
   let getSuppliersPromise = new UserDAO(firstDBtest).getSuppliers();
-  getSuppliersPromise.then(
+  await getSuppliersPromise.then(
     function(value) {
       console.log('getSuppliers resolve');
       return res.status(200).json(value).end();
@@ -75,12 +78,32 @@ app.get('/api/suppliers', (req, res) => {
     }
   ).catch(err => function(err){
     console.log('getUser error: ', err);
-    return res.status(500).end(err);
+    return res.status(500).end();
+  });
+});
+
+//GET /api/users
+app.get('/api/users', async (req, res) => {
+  console.log('GET /api/users');
+
+  let getUsersPromise = new UserDAO(firstDBtest).getUsers();
+  await getUsersPromise.then(
+    function(value) {
+      console.log('getUsers resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('getUsers reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err) {
+    console.log('getUsers error', err);
+    return res.status(500).end();
   });
 });
 
 //POST /api/newUser
-app.post('/api/newUser', (req, res) => {
+app.post('/api/newUser', async (req, res) => {
 
   console.log('POST /api/newUser');
 
@@ -93,8 +116,7 @@ app.post('/api/newUser', (req, res) => {
   //console.log(tmp_user_data);
 
   // Request Body Validation
-  const requiredKeys = ['name', 'surname', 'username', 'password', 'type']
-  const customerTypes = ['customer', 'qualityEmployee', 'clerk', 'deliveryEmployee', 'supplier'];
+  const requiredKeys = ['name', 'surname', 'username', 'password', 'type'];
 
   // checks passed number of fields is the required one
   if (Object.keys(tmp_user_data).length != requiredKeys.length) {
@@ -117,7 +139,7 @@ app.post('/api/newUser', (req, res) => {
       return res.status(422).end(); // 422 Unprocessable Entity
     }
 
-    if(!customerTypes.includes(tmp_user_data['type'])){
+    if(!userTypes.includes(tmp_user_data['type'])){
       return res.status(422).end(); // 422 Unprocessable Entity
     }
   }
@@ -125,7 +147,7 @@ app.post('/api/newUser', (req, res) => {
   let tmp_user = new User(tmp_user_data['name'], tmp_user_data['surname'], tmp_user_data['username'], tmp_user_data['password'], tmp_user_data['type']);
 
   const addUserPromise = new UserDAO(firstDBtest).newUser(tmp_user);
-  addUserPromise.then(
+  await addUserPromise.then(
     function(value) {
       console.log('addUser resolve');
       return res.status(value).end();
@@ -141,25 +163,356 @@ app.post('/api/newUser', (req, res) => {
   });
 });
 
-//POST /api/customerSessions
-app.post('/api/customerSessions', (req, res) => {
+//POST /api/managerSessions
+app.post('/api/managerSessions', async (req, res) => {
   // check input
   if (Object.keys(req.body).length === 0) {
     return res.status(422).json({
       error: 'Empty body request'
     });
   }
+  let managerData = req.body;
 
-  let customerInfo = {
-    id:1,
-    username:'test@ezwh.com',
-    name:'John',
-    surname:'Smith'
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(managerData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(managerData[key] == undefined || managerData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  managerData['type'] = 'manager';
+
+  let managerSessionsPromise = new UserDAO(firstDBtest).userSession(managerData);
+  await managerSessionsPromise.then(
+    function(value) {
+      console.log('managerSession resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('managerSession reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('managerSession catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//POST /api/customerSessions
+app.post('/api/customerSessions', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({
+      error: 'Empty body request'
+    });
+  }
+  let customerData = req.body;
+
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(customerData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(customerData[key] == undefined || customerData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  customerData['type'] = 'customer';
+
+  let customerSessionsPromise = new UserDAO(firstDBtest).userSession(customerData);
+  await customerSessionsPromise.then(
+    function(value) {
+      console.log('customerSession resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('customerSession reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('customerSession catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//POST /api/supplierSessions
+app.post('/api/supplierSessions', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({
+      error: 'Empty body request'
+    });
+  }
+  let supplierData = req.body;
+
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(supplierData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(supplierData[key] == undefined || supplierData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  supplierData['type'] = 'supplier';
+
+  let supplierSessionsPromise = new UserDAO(firstDBtest).userSession(supplierData);
+  await supplierSessionsPromise.then(
+    function(value) {
+      console.log('supplierSessions resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('supplierSessions reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('supplierSessions catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//POST /api/clerkSessions
+app.post('/api/clerkSessions', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({
+      error: 'Empty body request'
+    });
+  }
+  let clerkData = req.body;
+
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(clerkData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(clerkData[key] == undefined || clerkData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  clerkData['type'] = 'clerk';
+
+  let clerkSessionsPromise = new UserDAO(firstDBtest).userSession(clerkData);
+  await clerkSessionsPromise.then(
+    function(value) {
+      console.log('clerkSessions resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('clerkSessions reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('clerkSessions catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//POST /api/qualityEmployeeSessions
+app.post('/api/qualityEmployeeSessions', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({
+      error: 'Empty body request'
+    });
+  }
+  let qualityEmployeeData = req.body;
+
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(qualityEmployeeData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(qualityEmployeeData[key] == undefined || qualityEmployeeData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  qualityEmployeeData['type'] = 'qualityEmployee';
+
+  let qualityEmployeePromise = new UserDAO(firstDBtest).userSession(qualityEmployeeData);
+  await qualityEmployeePromise.then(
+    function(value) {
+      console.log('qualityEmployeeSessions resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('qualityEmployeeSessions reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('qualityEmployeeSessions catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//POST /api/deliveryEmployeeSessions
+app.post('/api/deliveryEmployeeSessions', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({
+      error: 'Empty body request'
+    });
+  }
+  let deliveryEmployeeData = req.body;
+
+  // Request Body Validation
+  const requiredKeys = ['username', 'password'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    // checks for necessary field presence
+    if(!Object.keys(deliveryEmployeeData).includes(key)){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+    // checks for fields not empty
+    if(deliveryEmployeeData[key] == undefined || deliveryEmployeeData[key] == ''){
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+  });
+  deliveryEmployeeData['type'] = 'deliveryEmployee';
+
+  let deliveryEmployeePromise = new UserDAO(firstDBtest).userSession(deliveryEmployeeData);
+  await deliveryEmployeePromise.then(
+    function(value) {
+      console.log('deliveryEmployeeSessions resolve');
+      return res.status(200).json(value).end();
+    },
+    function(error) {
+      console.log('deliveryEmployeeSessions reject');
+      return res.status(error).end();
+    }
+  ).catch(err => function(err){
+    console.log('deliveryEmployeeSessions catch error', err);
+    return res.status(500).end();
+  });
+});
+
+//PUT /api/users/:username
+app.put('/api/users/:username', async (req, res) => {
+  // check input
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).end();
   }
 
-  console.log('login received');
-  return res.status(200).json(customerInfo);
+  let userData = req.body;
+
+  // Request Body Validation
+  let valid = true;
+  const requiredKeys = ['oldType', 'newType'];
+
+  requiredKeys.forEach(key => {
+    //console.log(key);
+    
+    if(!Object.keys(userData).includes(key)){
+      // checks for necessary field presence
+      valid = false;
+      return res.status(422).end(); // 422 Unprocessable Entity
+    } else if(userData[key] == undefined || userData[key] == ''){
+      // checks for fields not empty
+      valid = false;
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }    
+  });
+  
+  if(valid) {
+    // checks for correct user types in request body
+    Object.values(userData).forEach(uType => {
+      if(!userTypes.includes(uType)) {
+        valid = false;
+        return res.status(422).end(); // 422 Unprocessable Entity
+      }
+    });
+    userData['username'] = req.params.username;
+
+    if(valid && (userData['username'] == undefined || userData['username'] == '' || userData['oldType'] == 'administrator' || userData['oldType'] == 'manager')) {
+      valid = false;
+      return res.status(422).end(); // 422 Unprocessable Entity
+    }
+
+    if(valid) {
+      let putUserPromise = new UserDAO(firstDBtest).modifyUserType(userData);
+      await putUserPromise.then(
+        function(value) {
+          console.log('PUTuser resolve');
+          return res.status(value).end();
+        },
+        function(error) {
+          console.log('PUTuser reject');
+          return res.status(error).end();
+        }
+      ).catch(err => function(err){
+        console.log('PUTuser error', err);
+        return res.status(503).end(); // 503 Service Unavailable (generic error)
+      });
+    }
+  }
 });
+
+//DELETE /api/users/:username/:type
+app.delete('/api/users/:username/:type', async (req, res) => {
+  let valid = true;
+  let userData = {
+    username:req.params.username,
+    type:req.params.type
+  };
+
+  if(!(typeof userData.username === 'string' && typeof userData.type === 'string') || 
+        userData.username == '' || userData.type == '' || userData.type == 'manager' ||
+        userData.type == 'administrator') {
+          valid = false;
+  } else if(!userTypes.includes(userData.type)){
+    // checks for correct user types in request body
+    valid = false;
+  }
+
+  if(!valid) {
+    return res.status(422).end(); // 422 Unprocessable Entity
+  } else {
+    let deleteUserPromise = new UserDAO(firstDBtest).deleteUser(userData);
+    await deleteUserPromise.then(
+      function(value) {
+        console.log('DELETEuser resolve');
+        return res.status(value).end();
+      },
+      function(error) {
+        console.log('DELETEuser reject');
+        return res.status(error).end();
+      }
+    ).catch(err => function(err) {
+      console.log('DELETEuser error', err);
+      return res.status(503).end(); // 503 Service Unavailable
+    });
+  }
+});
+
 
 // activate the server
 app.listen(port, () => {
