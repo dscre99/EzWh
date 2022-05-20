54a393364644181bd51589d4c10ef876ce7dabde
DIAMANTE SIMONE CRESCENZO s295783 <s295783@studenti.polito.it>
2022-05-20 15:45:14 +0200

    Chore: cleaned and removed some files. Added: testUserAPI.js. Mocha basic setup to procede with User API testing.

--- code/server/unit_test/position_unit_test/mock_position_dao.js
+++
@@ -1,5 +0,0 @@
-exports.getPositions = jest.fn();
-exports.storePosition = jest.fn();
-exports.put_position_by_ID_DB = jest.fn();
-exports.put_positionID_by_ID_DB = jest.fn();
-exports.delete_position_by_ID_DB = jest.fn();
