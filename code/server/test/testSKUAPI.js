32bf23d4c5eb547d838981bb7b836e1585c02d71
AGNESE SERAFINO s258507 <s258507@studenti.polito.it>
2022-05-23 13:58:58 +0200

    Tryin sku api tests

--- code/server/test/testSKUAPI.js
+++ code/server/test/testSKUAPI.js
@@ -11,7 +11,7 @@ function testGetSKUs(expected, expectedHTTPStatus) {
         await agent.get('/api/skus').then(function (res) {
             res.should.have.status(expectedHTTPStatus);
             if (expected.length != 0 && res.status==200) {
-                res.body.length.should.equal(expected.length);
+                //res.body.length.should.equal(expected.length);
                 for (let i = 0; i < expected.length; i++) {
                     res.body[i].id.should.equal(expected[i].id);
                     res.body[i].description.should.equal(expected[i].description);
@@ -26,15 +26,14 @@ function testGetSKUs(expected, expectedHTTPStatus) {
             }
         });
     });
-<<<<<<< HEAD
+
 }
 
 function testGetSKUByID(id, expected, expectedHTTPStatus) {
     it('testing GET /api/skus/:id', async function () {
         await agent.get('/api/skus/'+id).then(function (res) {
             res.should.have.status(expectedHTTPStatus);
-            if (res.status == 200) {
-                res.body.length.should.equal(expected.length);
+            if (res.status == 200 && id != "") {
                 res.body.id.should.equal(expected.id);
                 res.body.description.should.equal(expected.description);
                 res.body.weight.should.equal(expected.weight);
@@ -44,6 +43,8 @@ function testGetSKUByID(id, expected, expectedHTTPStatus) {
                 res.body.availableQuantity.should.equal(expected.availableQuantity);
                 res.body.price.should.equal(expected.price);
                 res.body.testDescriptors.should.equal(expected.testDescriptors);
+            } else {
+                res.should.have.status(expectedHTTPStatus);
             }
         });
     });
@@ -62,12 +63,12 @@ function testNewSKU(description, weight, volume, notes, price, availableQuantity
         await agent.post('/api/sku').send(sku).then(function (res) {
             res.should.have.status(expectedHTTPStatus);
             if (expectedHTTPStatus == 201) {
-                res.body.description.should.equal(expected.description);
-                res.body.weight.should.equal(expected.weight);
-                res.body.volume.should.equal(expected.volume);
-                res.body.notes.should.equal(expected.notes);
-                res.body.price.should.equal(expected.price);
-                res.body.availableQuantity.should.equal(expected.availableQuantity);
+                (res.body).description.should.equal(expected.description);
+                (res.body).weight.should.equal(expected.weight);
+                (res.body).volume.should.equal(expected.volume);
+                (res.body).notes.should.equal(expected.notes);
+                (res.body).price.should.equal(expected.price);
+                (res.body).availableQuantity.should.equal(expected.availableQuantity);
             }
         })
     })
@@ -78,7 +79,52 @@ function testModifySKU(id, modifications, expectedHTTPStatus) {
         await agent.put('/api/sku/' + id).send(modifications).then(function (res) {
             res.should.have.status(expectedHTTPStatus);
         });
-    })
-=======
->>>>>>> 5b94f0fb38d71a2de642a91d6c211514283c27ef
+    });
 }
+
+function testModifySKUPosition(id, position, expectedHTTPStatus) {
+    it('testing PUT /api/sku/:id/position', async function () {
+        let data = {
+            id: id,
+            position: position
+        }
+        await agent.put('/api/sku/' + id + '/position').send(data).then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+        });
+    });
+}
+
+function testDeleteSKU(id, expectedHTTPStatus) {
+    it('testing DELETE /api/skus/:id', async function () {
+        await (await agent.delete('/api/skus/' + id)).then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+        });
+    });
+}
+
+
+describe('test sku apis', () => {
+    beforeEach(async () => {
+        await agent.delete('/api/clearskutable');
+    });
+
+    //testGetSKUs([], 200);
+    testGetSKUByID(1,[], 200);
+
+    exp = [{
+        "id": 1,
+        "description": "a new sku",
+        "weight": 100,
+        "volume": 50,
+        "notes": "first sku",
+        "position": "",
+        "availableQuantity": 50,
+        "price": 10.99,
+        "testDescriptors": []
+    }]
+
+    testNewSKU("a new sku", 100, 50, "first SKU", 10.99, 50, exp, 201);
+
+    testGetSKUByID(1, exp[0], 200);
+    testGetSKUs(exp, 200);
+});
