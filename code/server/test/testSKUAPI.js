1c854f59c6f0552f806ebbc4b19f86b4740abe9b
AGNESE SERAFINO s258507 <s258507@studenti.polito.it>
2022-05-23 12:40:35 +0200

    Added some api checks

--- code/server/test/testSKUAPI.js
+++ code/server/test/testSKUAPI.js
@@ -10,7 +10,7 @@ function testGetSKUs(expected, expectedHTTPStatus) {
     it('testing GET /api/skus', async function () {
         await agent.get('/api/skus').then(function (res) {
             res.should.have.status(expectedHTTPStatus);
-            if (expected.length != 0) {
+            if (expected.length != 0 && res.status==200) {
                 res.body.length.should.equal(expected.length);
                 for (let i = 0; i < expected.length; i++) {
                     res.body[i].id.should.equal(expected[i].id);
@@ -22,9 +22,60 @@ function testGetSKUs(expected, expectedHTTPStatus) {
                     res.body[i].availableQuantity.should.equal(expected[i].availableQuantity);
                     res.body[i].price.should.equal(expected[i].price);
                     res.body[i].testDescriptors.should.equal(expected[i].testDescriptors);
-                    done();
                 }
             }
         });
-    }
+    });
+}
+
+function testGetSKUByID(id, expected, expectedHTTPStatus) {
+    it('testing GET /api/skus/:id', async function () {
+        await agent.get('/api/skus/'+id).then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+            if (res.status == 200) {
+                res.body.length.should.equal(expected.length);
+                res.body.id.should.equal(expected.id);
+                res.body.description.should.equal(expected.description);
+                res.body.weight.should.equal(expected.weight);
+                res.body.volume.should.equal(expected.volume);
+                res.body.notes.should.equal(expected.notes);
+                res.body.position.should.equal(expected.position);
+                res.body.availableQuantity.should.equal(expected.availableQuantity);
+                res.body.price.should.equal(expected.price);
+                res.body.testDescriptors.should.equal(expected.testDescriptors);
+            }
+        });
+    });
+}
+
+function testNewSKU(description, weight, volume, notes, price, availableQuantity, expected, expectedHTTPStatus) {
+    it('testing POST /api/sku', async function () {
+        let sku = {
+            description: description,
+            weight: weight,
+            volume: volume,
+            notes: notes,
+            price: price,
+            availableQuantity: availableQuantity
+        }
+        await agent.post('/api/sku').send(sku).then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+            if (expectedHTTPStatus == 201) {
+                res.body.description.should.equal(expected.description);
+                res.body.weight.should.equal(expected.weight);
+                res.body.volume.should.equal(expected.volume);
+                res.body.notes.should.equal(expected.notes);
+                res.body.price.should.equal(expected.price);
+                res.body.availableQuantity.should.equal(expected.availableQuantity);
+            }
+        })
+    })
+}
+
+function testModifySKU(id, modifications, expectedHTTPStatus) {
+    it('testing PUT /api/sku/:id', async function () {
+        await agent.put('/api/sku/' + id).send(modifications).then(function (res) {
+            res.should.have.status(expectedHTTPStatus);
+        });
+    })
 }
