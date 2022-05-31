a78c07dfd827d841112fd2ad04c36c16da98c525
dibbadan <daniele.dibattista@studenti.polito.it>
2022-05-31 23:55:04 +0200

    Test Result little fix

--- code/server/Test_result/Test_result.js
+++ code/server/Test_result/Test_result.js
@@ -121,7 +121,7 @@ class TestResultService {
         }
 
         this.#dao.delete_test_result_with_id_from_rfid_DB(req.params.id, req.params.rfid).then(() => {
-            res.status(200).json(`Test Result with id=${req.params.id} has been deleted!`);
+            res.status(204).json(`Test Result with id=${req.params.id} has been deleted!`);
         }).catch((error) => res.status(422).json(error));
     }
 }
