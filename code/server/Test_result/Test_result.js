95069f0cf8d6123701f8d86934dc7972d410ba8e
dibbadan <daniele.dibattista@studenti.polito.it>
2022-06-01 09:54:09 +0200

    Minor fixes

--- code/server/Test_result/Test_result.js
+++ code/server/Test_result/Test_result.js
@@ -23,21 +23,21 @@ class TestResultService {
 
         if(Number.parseInt(req.params.rfid) >= 0){
             if(!isAllowed(this.#loggedUser)) {
-                res.status(401).json("You are not allowed to see the test results!");
+                res.status(401).json("You are not allowed to see the test results!").end();
                 return;
             }
     
             if(!isEmpty(req.body)) {
-                res.status(503).json("Body must be empty!");
+                res.status(503).json("Body must be empty!").end();
                 return;
             }
     
     
             this.#dao.get_test_results_DB(req.params.rfid).then((test_results) => {
                 res.status(200).json(test_results);
-            }).catch((error) => res.status(404).json(error));
+            }).catch((error) => res.status(404).json(error).end());
         }else{
-            res.status(422).json("Unprocessable entity");
+            res.status(422).json("Unprocessable entity").end();
             return;
         }
         
@@ -47,42 +47,42 @@ class TestResultService {
     get_test_result_with_id_from_rfid = async(req,res) => {
         
         if(!isAllowed(this.#loggedUser)) {
-            res.status(401).json("You are not allowed!");
+            res.status(401).json("You are not allowed!").end();
             return;
         }
         if(!isEmpty(req.body)) {
-            res.status(503).json("Body must be empty!");
+            res.status(503).json("Body must be empty!").end();
             return;
         }
 
         if(req.params.rfid === 'null') {
-            res.status(422).json("Invalid rfid");
+            res.status(422).json("Invalid rfid").end();
             return;
         } 
         
         this.#dao.get_test_result_with_id_from_rfid_DB(req.params.id, req.params.rfid).then((test_result) => {
-            res.status(200).json(test_result);
-        }).catch((error) => res.status(404).json(error));
+            res.status(200).json(test_result).end();
+        }).catch((error) => res.status(404).json(error).end());
     }
 
     // // POST /api/skuitems/testResult
     post_test_result = async(req, res) => {
         if(req.body.rfid.length != 32){
-            res.status(422).json("Bad RFID");
+            res.status(422).json("Bad RFID").end();
             return;
         }
         if(!isAllowed(this.#loggedUser)) {
-            res.status(401).json("You are not allowed!");
+            res.status(401).json("You are not allowed!").end();
             return;
         }
         if(isEmpty(req.body)) {
-            res.status(422).json("Body is empty!");
+            res.status(422).json("Body is empty!").end();
             return;
         }
 
         this.#dao.post_test_result_DB(req.body).then((test_result) => {
-            res.status(201).json(test_result);
-        }).catch((error) => res.status(error).json(error))
+            res.status(201).json(test_result).end();
+        }).catch((error) => res.status(error).json(error).end())
     }
 
     // // PUT /api/skuitems/:rfid/testResult/:id
@@ -90,19 +90,19 @@ class TestResultService {
         
         if(Number.parseInt(req.params.rfid) >= 0){
             if(!isAllowed(this.#loggedUser)) {
-                res.status(401).json("You are not allowed!");
+                res.status(401).json("You are not allowed!").end();
                 return;
             }
             if(isEmpty(req.body)) {
-                res.status(422).json("Body is empty!");
+                res.status(422).json("Body is empty!").end();
                 return;
             }
             
             this.#dao.put_test_result_with_id_from_rfid_DB(req.params.id, req.params.rfid, req.body).then(() => {
                 res.status(200).json("Test Result updated!");
-            }).catch((error) => res.status(404).json(error));
+            }).catch((error) => res.status(404).json(error).end());
         }else{
-            res.status(422).json("Unprocessable entity");
+            res.status(422).json("Unprocessable entity").end();
             return;
         }
         
@@ -112,17 +112,17 @@ class TestResultService {
     delete_test_result_with_id_from_rfid = async(req, res) => {
 
         if(!isAllowed(this.#loggedUser)) {
-            res.status(401).json("You are not allowed!");
+            res.status(401).json("You are not allowed!").end();
             return;
         }
         if(!isEmpty(req.body)) {
-            res.status(503).json("Body must be empty!");
+            res.status(503).json("Body must be empty!").end();
             return;
         }
 
         this.#dao.delete_test_result_with_id_from_rfid_DB(req.params.id, req.params.rfid).then(() => {
-            res.status(204).json(`Test Result with id=${req.params.id} has been deleted!`);
-        }).catch((error) => res.status(422).json(error));
+            res.status(204).json(`Test Result with id=${req.params.id} has been deleted!`).end();
+        }).catch((error) => res.status(422).json(error).end());
     }
 }
 
